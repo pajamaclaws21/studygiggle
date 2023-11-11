5c0c161451110ba92af78e629ef4e69187db8c80
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2023-11-11 07:31:12 -0600

    trying to get the req.body thing working?

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var cors = require('cors');
 var app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 
 axios.defaults.headers.common['referer'] = 'https://studygiggle.onrender.com';
 
