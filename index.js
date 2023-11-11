0720398a06b478a93e5659dc56b176c1020a94e8
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2023-11-11 07:28:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var cors = require('cors');
 
 var app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 axios.defaults.headers.common['referer'] = 'https://studygiggle.onrender.com';
 
 app.get('/', function (req, res) {
